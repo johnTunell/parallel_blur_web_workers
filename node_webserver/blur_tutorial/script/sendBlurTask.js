660aaaaab5a2629039c7f76935686592d6ccffa1
DinoTrainer <tunell.john@gmail.com>
2016-12-08 11:16:30 +0100

    finished :D

--- node_webserver/blur_tutorial/script/sendBlurTask.js
+++ node_webserver/blur_tutorial/script/sendBlurTask.js
@@ -1,6 +1,6 @@
 // Send the blur task to the workers. Starts with setting up the needed data that each worker needs, then post a message to the worker with the data.
 
-function sendBlurTask(worker, i, chunkWidth, isFirst, isLast, isIterationBlur, iteration) {
+function sendBlurTask(worker, i, chunkWidth, isFirst, isLast, isIterationBlur, iteration, remainder) {
     const chunkHeight = image.height;
     const chunkStartX = i * chunkWidth;
     const chunkStartY = 0;
@@ -9,7 +9,7 @@ function sendBlurTask(worker, i, chunkWidth, isFirst, isLast, isIterationBlur, i
     // We need to make sure that each worker get the correct borders.
     const borderStartXleft = isFirst ? 0 : (i * chunkWidth) - 1;
     const imageBorderLeft = ctx.getImageData(borderStartXleft, chunkStartY, borderWidth, chunkHeight);
-    const borderStartXright = isLast ? ((i + 1) * chunkWidth) - 1 : ((i + 1) * chunkWidth);
+    const borderStartXright = isLast ? ((i + 1) * chunkWidth) - 1 + remainder: ((i + 1) * chunkWidth);
     const imageBorderRight = ctx.getImageData(borderStartXright, chunkStartY, borderWidth, chunkHeight);
 
     // Convert to arrayBuffers to send
--- node_webserver/blur_tutorial/script/worker_scripts/blurWorker.js
+++ node_webserver/blur_tutorial/script/worker_scripts/blurWorker.js
@@ -12,47 +12,55 @@ function messageHandler(e) {
     const messageType = e.data.type;
     switch (messageType) {
         case ("workerConstructor"):
-            this.sizeImageData = e.data.sizeImageData;
-            var typed_array = new Uint8ClampedArray(e.data.imageData,0, this.sizeImageData);
-            this.imageData = typed_array;
-            this.startX = e.data.startX;
-            this.width = e.data.width;
-            this.height = e.data.height;
+            workerConstructor(e);
             break;
         // When a new border arrives, the worker will blur one iteration, and then if it is the last iteration send back the finished image
         // It it is a iteration in the middle, it will send its new blurred borders, so that the segments next to it will have updated borders.
         case ("newBorder"):
-            var imageBorderLeft = new Uint8ClampedArray(e.data.imageBorderLeft,0, 1600);
-            var imageBorderRight = new Uint8ClampedArray(e.data.imageBorderRight,0, 1600);
-
-            const returnVal = boxBlur(this.imageData, this.width, this.height, imageBorderLeft, imageBorderRight);
-            this.imageData = returnVal.data;
-            const borderLeft = returnVal.borderRetLeft;
-            const borderRight = returnVal.borderRetRight;
-
-            // Convert to arraybuffers
-            var borderLeftBuf = new Uint8ClampedArray(borderLeft,0, 1600).buffer;
-            var borderRightBuf = new Uint8ClampedArray(borderRight,0, 1600).buffer;
-
-                if(e.data.iteration === 30) {
-                    var imageData= new Uint8ClampedArray(this.imageData,0, this.sizeImageData).buffer;
-                    postMessage({
-                        "type": "finished",
-                        "imageData": imageData,
-                        "width": this.width,
-                        "height": this.height,
-                        "startX": this.startX,
-                        "iteration": e.data.iteration,
-                        "sizeImageData": this.sizeImageData
-                    }, [imageData]);
-                } else {
-                    postMessage({
-                        "type": "partialResult",
-                        "iteration": e.data.iteration,
-                        'imageBorderRight': borderRightBuf,
-                        'imageBorderLeft': borderLeftBuf
-                    }, [borderLeftBuf, borderRightBuf]);
-            }
+            newBorder(e);
     }
 }
 addEventListener("message", messageHandler, true);
+
+function workerConstructor(e) {
+    this.sizeImageData = e.data.sizeImageData;
+    var typed_array = new Uint8ClampedArray(e.data.imageData,0, this.sizeImageData);
+    this.imageData = typed_array;
+    this.startX = e.data.startX;
+    this.width = e.data.width;
+    this.height = e.data.height;
+}
+
+function newBorder(e) {
+    var imageBorderLeft = new Uint8ClampedArray(e.data.imageBorderLeft,0, this.height*4);
+    var imageBorderRight = new Uint8ClampedArray(e.data.imageBorderRight,0, this.height*4);
+
+    const returnVal = boxBlur(this.imageData, this.width, this.height, imageBorderLeft, imageBorderRight);
+    this.imageData = returnVal.data;
+    const borderLeft = returnVal.borderRetLeft;
+    const borderRight = returnVal.borderRetRight;
+
+    // Convert to arraybuffers
+    var borderLeftBuf = new Uint8ClampedArray(borderLeft,0, this.height*4).buffer;
+    var borderRightBuf = new Uint8ClampedArray(borderRight,0, this.height*4).buffer;
+
+    if(e.data.iteration === 30) {
+        var imageData= new Uint8ClampedArray(this.imageData,0, this.sizeImageData).buffer;
+        postMessage({
+            "type": "finished",
+            "imageData": imageData,
+            "width": this.width,
+            "height": this.height,
+            "startX": this.startX,
+            "iteration": e.data.iteration,
+            "sizeImageData": this.sizeImageData
+        }, [imageData]);
+    } else {
+        postMessage({
+            "type": "partialResult",
+            "iteration": e.data.iteration,
+            'imageBorderRight': borderRightBuf,
+            'imageBorderLeft': borderLeftBuf
+        }, [borderLeftBuf, borderRightBuf]);
+    }
+}
